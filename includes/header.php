94ab76f6b6fd6bcbd3e022cb57c08a2b164f0f1e
Himan23 <136013871+HawkeyeAKAMihawk@users.noreply.github.com>
2024-11-16 15:00:58 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,24 +6,10 @@
   	<title>Library System using PHP</title>
   	<!-- Tell the browser to be responsive to screen width -->
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  	<!-- Bootstrap 3.3.7 -->
-  	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  	<!-- DataTables -->
-    <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-  	<!-- Font Awesome -->
-  	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  	<!-- Theme style -->
-  	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  	<!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  	<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  	
+  	
 
-  	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  	<!--[if lt IE 9]>
-  	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  	<![endif]-->
+  	
 
   	<!-- Google Font -->
   	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
@@ -56,9 +42,9 @@
     <link href="dist/lib/animate/animate.min.css" rel="stylesheet">
     <link href="dist/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-    <!-- Customized Bootstrap Stylesheet -->
+    
     <link href="dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Template Stylesheet -->
+   
     <link href="dist/css/style.css" rel="stylesheet">
 </head>
