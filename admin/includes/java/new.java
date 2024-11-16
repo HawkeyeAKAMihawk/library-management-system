fd8255ebf31ffb6f3de9605c2ee01b3dc664b78b
Himan23 <136013871+HawkeyeAKAMihawk@users.noreply.github.com>
2024-11-16 14:54:13 +0530

    Add files via upload

---
+++ admin/includes/java/new.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+class animal{
+
+    Scanner scanner  = new Scanner(System.in);
+
+    int age  =  scanner.nextInt(90);
+    scanner.close();
+
+}
+
+public class new{
+    public static void main(String args[]){
+             animal a = new animal();
+             a.age = 9;
+
+
+    }
+}
